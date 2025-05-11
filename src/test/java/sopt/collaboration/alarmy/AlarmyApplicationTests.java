3cb3ccb21af240ca4dbec04784309ded542006fb
ose0221 <ose0221@naver.com>
2025-05-11 22:12:14 +0900

    🎉[INIT] 프로젝트 초기 세팅

---
+++ src/test/java/sopt/collaboration/alarmy/AlarmyApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.collaboration.alarmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
