0cf450dbb1468c8c6b27c128d5a2b387d275095f
LSM <leesung2925@gmail.com>
2025-05-12 16:39:05 +0900

    :sparkles: [FEAT] #3 도메인 생성 및 연관 관계 설정

---
+++ src/main/java/sopt/collaboration/alarmy/phrase/domain/Phrase.java
@@ -0,0 +1,15 @@
+package sopt.collaboration.alarmy.phrase.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Phrase {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+}
